ff6d5b24843bbcb87dd9b03495b39bf8e3169755
Peter Conley <peterdavidconley@gmail.com>
2021-12-17 11:08:20 -0500

    logout properly working

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Logout = () => {        
+const Logout = (props) => {  
+    
+    const { push } = useHistory();
+
+    useEffect(() => {
+        axiosWithAuth()
+        .post('/logout')
+        .then(resp => {
+            localStorage.removeItem('token');
+            push('/login');
+        })
+    }, [])
+    
     return(<div></div>);
 }
 
