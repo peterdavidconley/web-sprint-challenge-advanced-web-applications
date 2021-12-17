3e7bba1420d69829e089b71b2585c94125f3cee8
Peter Conley <peterdavidconley@gmail.com>
2021-12-17 13:19:48 -0500

    wrote handle edit

--- src/components/View.js
+++ src/components/View.js
@@ -24,6 +24,13 @@ const View = (props) => {
     }
 
     const handleEdit = (article) => {
+        axios.get(`http://localhost:5000/api/articles/:id`, article)
+        .then(resp => {
+            console.log(resp)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     const handleEditSelect = (id)=> {
